0c390e7e3ba83b049826c4f64dc11de70a223dfb
John Marie Ygot <johnmarieygot21@gmail.com>
2025-01-26 10:59:40 +0800

    fix: changed the description of personal porfolio project

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -19,7 +19,7 @@ export const projects: Projects[] = [
   {
     name: "Personal Portfolio",
     description:
-      "SWU-ETS is a capstone project designed to assist event organizers and judges in efficiently managing and streamlining the process of events, such as pageants and contests. The system allows admins and organizers to effortlessly create and delete events, providing a user-friendly interface for event management.",
+      "My personal portfolio is a collection of projects and work that showcase my skills as a developer. It highlights the web applications I've built, the technologies I've worked with, and gives you a look at what I can do. It's a place where you can learn more about my journey and see some of the things I'm passionate about in development.",
     image: "/images/portfolio.png",
     url: "https://github.com/jmarie21/Personal-Portfolio",
     tools: ["NextJs", "Typescript", "ShadcnUI"],
